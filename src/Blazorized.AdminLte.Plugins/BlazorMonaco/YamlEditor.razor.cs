0717a830fe6114cdfe85b82775ee10f0647ce5fc
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2020-10-11 17:52:23 +0200

    Yaml Editor

---
+++ src/Blazorized.AdminLte.Plugins/BlazorMonaco/YamlEditor.razor.cs
@@ -0,0 +1,33 @@
+﻿using BlazorMonacoYaml;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace Blazorized.AdminLte.Plugins
+{
+    public partial class YamlEditor
+    {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        private MonacoEditorYaml _yamlEditor { get; set; }
+
+        [Parameter]
+        public string Value { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await JSRuntime.InvokeAsync<object>("splitYamlContentEditor", new object[] { DotNetObjectReference.Create(this), "InvokeLayout" }).ConfigureAwait(false);
+            }
+            await base.OnAfterRenderAsync(firstRender).ConfigureAwait(false);
+        }
+
+        [JSInvokable("InvokeLayout")]
+        public Task Layout()
+        {
+            return _yamlEditor.Layout();
+        }
+    }
+}
--- src/Blazorized.AdminLte.Plugins/wwwroot/js/split.interop.js
+++ src/Blazorized.AdminLte.Plugins/wwwroot/js/split.interop.js
@@ -1,4 +1,40 @@
-﻿var blazorSplitter = (function () {
+﻿/// <reference path="split.js" />
+function splitYamlContentEditor(dotNetReference, onResizeCallBackMethodName) {
+    OnResizeCallBackMethodName = onResizeCallBackMethodName;
+    OnResizeCallBackReference = dotNetReference;
+    $(window).resize(function () {
+        var window_height = $(window).height(),
+            header_height = $(".main-header").height() + $(".main-footer").height();
+
+        var containerHeight = window_height - header_height - 34
+        containerHeight = $(".content-wrapper").height();
+        $("#splitcontainer").css("height", containerHeight);
+        var tabHeight = containerHeight - 56; //minus tab selectors height
+        $("#splitcontainer .tab-content").css("height", tabHeight);
+        $("#splitcontainer .tab-content .tab-pane").css("height", tabHeight);
+        if (dotNetReference && onResizeCallBackMethodName) {
+            OnResizeCallBackReference.invokeMethodAsync(onResizeCallBackMethodName);
+        }
+    });
+    var minSize = 310;
+    Split(["#left-half_contenteditor", "#right-half_contenteditor"], {
+        elementStyle: function (dimension, size, gutterSize, index) {
+            $(window).trigger('resize');
+            if (index === 0)
+                return { 'flex-basis': 'calc(100% - 10px - max(calc(' + (100 - size) + '% - ' + gutterSize + 'px), ' + (minSize + gutterSize) + 'px))' }
+            if (index === 1)
+                return { 'flex-basis': 'max(calc(' + size + '% - ' + gutterSize + 'px), ' + (minSize + gutterSize) + 'px)' }
+
+        },
+        gutterStyle: function (dimension, gutterSize) { return { 'flex-basis': gutterSize + 'px' } },
+        sizes: [80, 20],
+        minSize: minSize,
+        gutterSize: 6,
+        cursor: 'col-resize'
+    });
+}
+
+var blazorSplitter = (function () {
     //menu hamburger resize will do callback to redraw the yamllayouteditor
     let OnResizeCallBackMethodName;
     let OnResizeCallBackReference;
@@ -83,9 +119,3 @@
     };
 })();
 
-window.console && console.log(
-    myApp.next(),
-    myApp.next(),
-    myApp.reset(),
-    myApp.next()
-) //0, 1, undefined, 0 
