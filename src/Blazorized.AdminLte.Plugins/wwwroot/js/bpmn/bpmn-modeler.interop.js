62f2b91a9dea2202ad4b7fa1e434b5beeca688ee
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2021-01-20 17:59:58 +0100

    add bpmn modeler and icons

---
+++ src/Blazorized.AdminLte.Plugins/wwwroot/js/bpmn/bpmn-modeler.interop.js
@@ -0,0 +1,46 @@
+﻿/// <reference path="bpmn-modeler.min.js" />
+window.bpmnModeler = {
+
+    modelers: new Object(),
+
+    create: function (namedInstance, canvas) {
+        this.modelers[namedInstance] = new BpmnJS({
+            container: canvas,
+            keyboard: {
+                bindTo: window
+            }
+        });
+    },
+
+    exportDiagram: function (namedInstance) {
+        this.modelers[namedInstance].saveXML({ format: true }, function (err, xml) {
+            if (err) {
+                return console.error('could not save BPMN 2.0 diagram', err);
+            }
+            alert('Diagram exported. Check the developer tools!');
+            console.log('DIAGRAM', xml);
+        });
+    },
+
+    openDiagram: function(namedInstance, bpmnXML) {
+        // import diagram
+        this.modelers[namedInstance].importXML(bpmnXML);
+            /*
+            // access modeler components
+            var canvas = this.modelers[namedInstance].get('canvas');
+            var overlays = this.modelers[namedInstance].get('overlays');
+            // zoom to fit full viewport
+            canvas.zoom('fit-viewport');
+            // attach an overlay to a node
+            overlays.add('SCAN_OK', 'note', {
+                position: {
+                    bottom: 0,
+                    right: 0
+                },
+                html: '<div class="diagram-note">Mixed up the labels?</div>'
+            });
+            // add marker
+            canvas.addMarker('SCAN_OK', 'needs-discussion');
+            */
+    }
+}
